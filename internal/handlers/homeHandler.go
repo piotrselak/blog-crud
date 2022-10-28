72fdadc604f7911c24d0f4027421de5e1559388d
piotr.selak <p.selak.848@studms.ug.edu.pl>
2022-10-28 21:15:08 +0200

    Mongo integration

--- internal/handlers/homeHandler.go
+++ internal/handlers/homeHandler.go
@@ -4,8 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func HomeHandler(c *gin.Context) {
-	c.String(http.StatusOK, "Front page") // placeholder for now
+func HomeHandler(client *mongo.Collection) gin.HandlerFunc {
+	handler := func(c *gin.Context) {
+		c.String(http.StatusOK, "Front page") // placeholder for now
+	}
+	return gin.HandlerFunc(handler)
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,11 +2,15 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/piotrselak/blog-crud/internal/db"
 	"github.com/piotrselak/blog-crud/internal/handlers"
 )
 
 func RunRoutes() {
+	client := db.InitializeMongoDB()
+	postsCollection := client.Database("blog").Collection("posts")
+
 	routes := gin.Default()
-	routes.GET("/", handlers.HomeHandler)
+	routes.GET("/", handlers.HomeHandler(postsCollection))
 	routes.Run()
 }
